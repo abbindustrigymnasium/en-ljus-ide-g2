d3ab916b05dcf1ce9cf939c07b614aad66f50fd3
Samuel Olsson <43879730+abbsamols@users.noreply.github.com>
2018-11-27 11:01:03 +0100

    Update Component1.js

--- Frontend'/app/components/screen1/Component1.js
+++ Frontend'/app/components/screen1/Component1.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import { 
+import React from 'react'; //Importerar den här komponenten från React.
+import { //Bestämmer vad som ska importeras för att användas i den här komponenten där man kan tända lampan.
 	StyleSheet,
 	View,
 	Text,
 	TouchableOpacity,
-} from 'react-native';
+} from 'react-native'; //StyleSheet, View, Text och TouchableOpacity importeras från react-native.
 
 
 export default class Component1 extends React.Component {
 
+constructor(props)
+{
+
+	super(props)
+	this.state= {
+		lightName: '',
+		Strength: ''	
+	}
+}
+
+InsertDataToServer =() => {
+
+	const {lightName} = this.state;
+	const {Str} = this.state;
+
+		if (lightName='') { //Om lampan är släckt. (Lampan är alltid släckt i den första skärmen, screen1.)
+		fetch('http://192.168.0.104:3001/light/'+lightName,{ //Bestämmer vart det nya värdet hamnar.
+			method: 'POST', //Bestämmer att ett nytt värde skapas.
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ //Gör om det som skickas till json-format.
+				lightName: lightName, //Bestämmer att lightName ska heta lightName på http://192.168.0.104:3001/light.
+				Str: Strength ////Bestämmer det ska stå Strength istället för Str på http://192.168.0.104:3001/light.
+			})
+		}).then((response) => response.json()).then(responseJSON => { //Gär om responsen till json-format.
+
+			console.log(responseJSON); //GÖr så att vi ser om värdet har skickats.
+			alert(responseJSON.message+'	'+ lightName); //Visar om det gick att skicka det nya värdet.
+
+		}).catch((error)=>{
+			console.log(error); //Meddelar vad som är fel.
+		});
+		}
+}
 
     render() {
 
@@ -33,7 +69,7 @@ export default class Component1 extends React.Component {
 
                                 <TouchableOpacity 
 										style={styles.item1}
-										onPress={() => this.props.navigation.navigate('Screen2', {})}
+										onPress={() => this.props.navigation.navigate('Screen2', {})} //Om man trycker på knappen kommer man till den andra skärmen där man kan ändra ljusets värme och ljusstryka.
 									>
 										
 										<Text style={styles.item1TouchableOpacity}>
